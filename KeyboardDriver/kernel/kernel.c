d2e5af4d0cf0ff291fcf3b3c36446016571aad17
이민성 <133635509+minsung-phy@users.noreply.github.com>
2024-02-23 16:34:43 +0900

    Add files via upload

---
+++ KeyboardDriver/kernel/kernel.c
@@ -0,0 +1,19 @@
+#include "../cpu/idt.h"
+#include "../cpu/isr.h"
+#include "../cpu/timer.h"
+#include "../drivers/display.h"
+#include "../drivers/keyboard.h"
+
+#include "util.h"
+
+void start_kernel() {
+    clear_screen();
+    print_string("Installing interrupt service routines (ISRs).\n");
+    isr_install();
+
+    print_string("Enabling external interrupts.\n");
+    asm volatile("sti");
+
+    print_string("Initializing keyboard (IRQ 1).\n");
+    init_keyboard();
+}
